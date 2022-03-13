c108006f8797b35ec42c740fe4fb5fa4c6327ba2
vaccine1208 <chimyassin@gmail.com>
2022-03-13 15:14:02 +0100

    more_changes

---
+++ app/Models/files.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class files extends Model
+{
+    //choosing specific table
+    protected $table = 'files';
+}
